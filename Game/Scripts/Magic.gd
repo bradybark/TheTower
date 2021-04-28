1007630c9568f1327b0c15b2e065b62b7b57510c
bradybark <82960817+bradybark@users.noreply.github.com>
2021-04-28 14:41:39 -0400

    Update Magic.gd

--- Game/Scripts/Magic.gd
+++ Game/Scripts/Magic.gd
@@ -6,4 +6,4 @@ func _on_AnimationPlayer_animation_finished(_anim_name):
 
 func player_lose_mp():
 	if BattleUnits.PlayerStats.mp >= 2:
-		BattleUnits.PlayerStats.mp1 -= 2
+		BattleUnits.PlayerStats.mp -= 2
